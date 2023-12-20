5754af74b149c1361ed01f5c892c45504ac3d964
rizelliot <filipkozuchowski@gmail.com>
2023-12-20 16:23:43 +0100

    fix parenthesis bug

--- websockets/index.js
+++ websockets/index.js
@@ -1,17 +1,15 @@
 const WebS = require("ws")
-
 const wss = new WebS.Server({port:8085})
 
-wss.on("connection", ws =>{
-  console.log("yeah")
-  ws.on("message", msg => {
-    wss.broadcast(JSON.stringify({func:msg.toString()}))
-
-  })
+wss.on("connection",ws=>{
+    console.log("connection!")
+    ws.on("message",msg=>{
+        wss.broadcast(JSON.stringify({func:msg.toString()}))
+    })
 });
 
 wss.broadcast = function broadcast(msg){
     wss.clients.forEach(function each(client) {
-      client.send(msg)
-    })
-}
+        client.send(msg)
+    });
+};
